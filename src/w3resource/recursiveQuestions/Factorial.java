15cba7f9b93111798408ed7af0967bfaee66ee28
Joy Joseph <joy828545@gmail.com>
2023-12-18 19:20:17 +0100

    git commit -m "calculating Factorial using recursion"

---
+++ src/w3resource/recursiveQuestions/Factorial.java
@@ -0,0 +1,16 @@
+package w3resource.recursiveQuestions;
+
+public class Factorial {
+    public int calculateFactorial(int number){
+        if (number == 0) return 1;
+        return number * calculateFactorial(number - 1);
+    }
+
+    public static void main(String[] args) {
+        Factorial factor = new Factorial();
+        System.out.println(factor.calculateFactorial(12));
+        int factorial = factor.calculateFactorial(7);
+        System.out.println(factorial);
+
+    }
+}
